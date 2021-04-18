7b5992ce11e8630e4f57bc5c06f8d121ef2ea57b
Niklas Gustafsson <niklas.m.gustafsson@gmail.com>
2021-04-18 18:53:37 +0200

    working code

--- src/App.tsx
+++ src/App.tsx
@@ -7,40 +7,50 @@ import InputBox from 'components/InputBox';
 import OutputBox from 'components/OutputBox';
 
 
+interface IData {
+  numWords: number,
+  numLetters: number
+}
 
 function App() {
   const [data, setData] = useState("");
-  const [result2, setResult] = useState({})
-  var noanswer : boolean = false;
-  var result: { numWords: number; numLetters: number };
+  const [numLetters, setLetters] = useState(Number);
+  const [numWords, setWords] = useState(Number);
+  var noanswer: boolean = false;
+
+
+  let result: { numWords: number; numLetters: number };
 
   const clickAlert = async () => {
-    return result = await analyzeText(data);
+    console.log(data);
+    await analyzeText(data).then( value => {
+
+      console.log("analyse", value)
+      setLetters(value.numLetters);
+      setWords(value.numWords);
+    });
+    // setResult(result);
+
   }
+  // console.log(result2);
 
-  // const renderQuestionOrAnswer = () => {
-  //   if (isLoggedIn) {
-  //     return <InputBox></InputBox>;
-  //   } else {
-  //     return <OutputBox></OutputBox>;
-  //   }
-  // }
-
-  // const fetchData = async () =>  {
+  // const fetchData = async () => {
   //   input = await analyzeText(testData);
   // };
   // fetchData();
 
-    return (
-      <div className="App">
-          <header className="App-header">
-          {result2 ?
-          <InputBox handleClick={(e) => clickAlert(e)}></InputBox> :
-          <OutputBox text="hej"></OutputBox>
-          }
-          </header>
-      </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        {numLetters === 0 ?
+        <>
+          <TextField id="standard-basic" onChange={e => setData(e.target.value)}></TextField>
+			    <Button variant="contained" onClick={() => clickAlert()}>Submit</Button>
+        </>: <p>Your text consist of {numWords} words ({numLetters} letters)</p>
+        }
+      </header>
+    </div>
+  );
 }
 
 export default App;
